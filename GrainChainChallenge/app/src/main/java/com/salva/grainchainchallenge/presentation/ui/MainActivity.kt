fa226f51d870c2878245fac9a8de65e0f2b339f1
Salvador Munguia <salvamunguiaespinoza@gmail.com>
2021-12-14 14:58:38 -0600

    firts commit

---
+++ GrainChainChallenge/app/src/main/java/com/salva/grainchainchallenge/presentation/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.salva.grainchainchallenge.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils.replace
+import com.salva.grainchainchallenge.R
+import com.salva.grainchainchallenge.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       supportFragmentManager.beginTransaction().add(R.id.main_container,MapsFragment()).commit()
+
+    }
+}
